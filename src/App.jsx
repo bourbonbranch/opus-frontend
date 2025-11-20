dbca4b00e5a0e642c4554f880d0ba421097fd7f4
Jacob Cochran <jacobcochran@Jacobs-MacBook-Pro.local>
2025-11-19 21:32:52 -0600

    Remove nested BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import SignUp from './pages/SignUp';
 import AddEnsemble from './pages/AddEnsemble';
@@ -39,32 +39,30 @@ class ErrorBoundary extends React.Component {
 const App = () => {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <Routes>
-          {/* Signup */}
-          <Route path="/" element={<SignUp />} />
-          <Route path="/signup" element={<SignUp />} />
+      <Routes>
+        {/* Signup */}
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
 
-          {/* Create Ensemble */}
-          <Route path="/add-ensemble" element={<AddEnsemble />} />
-          <Route path="/ensembles/new" element={<AddEnsemble />} />
+        {/* Create Ensemble */}
+        <Route path="/add-ensemble" element={<AddEnsemble />} />
+        <Route path="/ensembles/new" element={<AddEnsemble />} />
 
-          {/* Dashboard */}
-          <Route path="/director/today" element={<TodayDashboard />} />
+        {/* Dashboard */}
+        <Route path="/director/today" element={<TodayDashboard />} />
 
-          {/* Roster page for a specific ensemble */}
-          <Route
-            path="/ensembles/:ensembleId/roster"
-            element={<Roster />}
-          />
+        {/* Roster page for a specific ensemble */}
+        <Route
+          path="/ensembles/:ensembleId/roster"
+          element={<Roster />}
+        />
 
-          {/* Catch-all */}
-          <Route
-            path="*"
-            element={<Navigate to="/director/today" replace />}
-          />
-        </Routes>
-      </BrowserRouter>
+        {/* Catch-all */}
+        <Route
+          path="*"
+          element={<Navigate to="/director/today" replace />}
+        />
+      </Routes>
     </ErrorBoundary>
   );
 };
